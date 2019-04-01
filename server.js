b381b868ea8c4931b58ea698a86f5732a3429fe3
Hunter Houston <hunter.houston@meltwater.com>
2019-03-31 23:43:57 -0700

    Mongo set up. server and dependencies WIP

--- server.js
+++ server.js
@@ -1,17 +1,77 @@
 import express from 'express'
+import Koa from 'koa'
 import graphqlHTTP from 'express-graphql'
 // import gql from 'graphql-tag'
 import { makeExecutableSchema } from 'graphql-tools'
 import cors from 'cors'
 
-import { ApolloServer, gql } from 'apollo-server'
+import { ApolloServer, gql } from 'apollo-server-koa'
 
 import { Schema, Photo } from './lib/graphql'
 import { Query, resolvers } from './lib/graphql'
 
-console.log('Photo: ', Photo);
-console.log('Query: ', Query);
-console.log('resolvers: ', resolvers);
+import { MongoDB } from './lib/mongodb'
+
+import { createContainer } from 'awilix'
+import { scopePerRequest } from 'awilix-koa'
+
+import { createDependencies } from './lib'
+import { MongoService } from './lib/services'
+
+console.log('service: ', MongoService)
+const dependencies = createDependencies()
+console.log('depppp: ', dependencies)
+
+const schema = makeExecutableSchema({
+  typeDefs: [Query, Photo],
+  resolvers
+})
+
+console.log('schema::: %j', Schema);
+
+const server = new ApolloServer({
+  schema: Schema
+})
+
+const app = new Koa();
+// app.use(cors())
+
+
+app.use(scopePerRequest(dependencies))
+
+// app.use((ctx, next) => {
+//   // ctx.scope = container.createScope()
+//
+//   ctx.scope.register({
+//
+//     dependencies
+//   })
+//
+//   return next()
+// })
+
+console.log('apppp', app)
+
+server.applyMiddleware({ app })
+
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
+)
+
+
+
+
+
+
+
+
+// const mongoClient = new MongoDB
+// console.log('mongoClient', mongoClient.start());
+//
+// console.log('Photo: ', Photo);
+// console.log('Query: ', Query);
+// console.log('resolvers: ', resolvers);
 // console.log('schema: ', Schema);
 
 // var { buildSchema } = require('graphql');
@@ -44,10 +104,7 @@ console.log('resolvers: ', resolvers);
 //   Query
 // ]
 
-const schema = makeExecutableSchema({
-  typeDefs: [Query, Photo],
-  resolvers
-})
+
 //
 // // The root provides a resolver function for each API endpoint
 // const root = {
@@ -67,8 +124,10 @@ const schema = makeExecutableSchema({
 //   },
 // };
 //
-// var app = express();
+// const app = express();
 // app.use(cors())
+//
+// server.applyMiddleware({ app })
 // app.use('/graphql', graphqlHTTP({
 //   schema: schema,
 //   rootValue: root,
@@ -94,11 +153,3 @@ const schema = makeExecutableSchema({
 
 // Resolvers define the technique for fetching the types in the
 // schema.  We'll retrieve books from the "books" array above.
-
-const server = new ApolloServer({
-  schema: Schema
-})
-
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-})
