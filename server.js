8a5e58f87e9cbfb0281b2e8be2f0dca621889ef5
Hunter Houston <hunter.houston@meltwater.com>
2019-04-29 11:06:53 -0700

    File upload working (needs polishing)

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 import Koa from 'koa'
 import { apolloUploadKoa } from 'apollo-upload-server'
 
-import { makeExecutableSchema } from 'graphql-tools'
-import { ApolloServer, gql } from 'apollo-server-koa'
+import { ApolloServer, gql, GraphQLUpload } from 'apollo-server-koa'
 import cors from '@koa/cors'
 
 import { createContainer } from 'awilix'
@@ -17,14 +16,30 @@ const dependencies = createDependencies()
 
 const app = new Koa();
 app.use(cors())
-app.use(apolloUploadKoa({ maxFileSize: 10000000, maxFiles: 10 }))
+// app.use(apolloUploadKoa({ maxFileSize: 10000000, maxFiles: 10 }))
 
 const server = new ApolloServer({
   typeDefs,
   resolvers: {
       Query: dependencies.resolve('Query'),
-      Mutation: dependencies.resolve('Mutation')
-   }
+      Mutation: dependencies.resolve('Mutation'),
+      Upload: GraphQLUpload
+   },
+   formatError: error => {
+      console.log(error);
+      return new Error('Internal server error');
+      // Or, you can delete the exception information
+      // delete error.extensions.exception;
+      // return error;
+    },
+   uploads: {
+      // Limits here should be stricter than config for surrounding
+      // infrastructure such as Nginx so errors can be handled elegantly by
+      // graphql-upload:
+      // https://github.com/jaydenseric/graphql-upload#type-uploadoptions
+      maxFileSize: 10000000, // 10 MB
+      maxFiles: 20
+    }
 })
 
 server.applyMiddleware({ app })
